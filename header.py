3a297992b1f204e197eb809c9e146138dbed0f52
James Kirkham <james.kirkham@arm.com>
2022-07-26 08:57:57 -0500

    Serialization

--- header.py
+++ header.py
@@ -1,21 +1,55 @@
-import numpy as np 
+import struct
 
-class Response:
-    def __init__(self):
-        self.MagicNumber = np.uint32(0x5EC0A710)
-        self.HeaderSize = np.uint16()  
-        self.MajorVersionNumber = np.uint8(0x01)
-        self.minorVersionNumber = np.uint8(0x00)
-        self.flags = np.uint16(0x00)
-        self.provider = np.uint8()
-        self.sessionHandle = np.uint64()
-        self.contentType = np.uint8(0x00)
-        self.acceptType = np.uint8(0x00)
-        self.authType = np.uint8()
-        self.contentLength = np.uint32()
-        self.authLength = np.uint16()
-        self.opcode = np.uint32()
-        self.status = np.uint16(0x00) #response only
-        self.reserved = np.uint16(0x00)
+class Header:
+    def default_in(self):
+        self.MagicNumber = 0x5EC0A710
+        self.HeaderSize = 0
+        self.MajorVersionNumber = 0
+        self.minorVersionNumber = 0
+        self.flags = 0
+        self.provider = 0
+        self.sessionHandle = 0
+        self.contentType = 0
+        self.acceptType = 0
+        self.authType = 0
+        self.contentLength = 0
+        self.authLength = 0
+        self.opcode = 1
+        self.status = 0 #response only
+        self.reserved = 0
 
         pass
+
+    def __init__(self, provider, sessionHandle, authType, contentLength, opCode):
+        self.default_in()
+
+        self.provider = 0
+        self.sessionHandle = 0
+        self.authType = 0
+        self.contentLength = 0
+        self.opcode = 0
+
+        self.headerSize = 1
+        self.authLength = 1
+
+    
+    def serialize(self):
+        return struct.pack('<IHBBHBQBBBIHIHH',
+        self.MagicNumber,
+        self.HeaderSize, 
+        self.MajorVersionNumber, 
+        self.minorVersionNumber, 
+        self.flags, 
+        self.provider,
+        self.sessionHandle, 
+        self.contentType,
+        self.acceptType,
+        self.authType,
+        self.contentLength,
+        self.authLength,
+        self.opcode,
+        self.status,
+        self.reserved)
+
+    def deserialize(self, serialized_header):
+        return struct.unpack('<IHBBHBQBBBIHIHH', serialized_header)
--- request.py
+++ request.py
@@ -1,7 +1,37 @@
+import pickle
+import struct
+from header import Header
+
 class Request:
     def __init__(self, opcode, providerId, auth):
         self.opcode = opcode
         self.providerId = providerId
         self.auth = auth
+        self.header = None
+
+    def build_operation(self, opcode):
+        if opcode == 1:
+            op_header = Header(0, 1, 0, 0, 1)
+            self.header = op_header
+
+    def get_header(self):
+        return self.header
+
     def send(self):
         pass
+
+
+if __name__ == "__main__":
+    req = Request(1, 0, 0)
+
+    req.build_operation(1)
+    
+    my_header = req.header
+    serialized_header = req.header.serialize()
+
+    print("Serialized header:", serialized_header)
+    
+    deserialized_header = req.header.deserialize(serialized_header)
+
+    print("Deserialized header:", deserialized_header)
+    
